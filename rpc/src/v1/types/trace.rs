248f48836b8877f6e743882cb8fd7acfa2ebf260
debris <marek.kotewicz@gmail.com>
2016-04-25 13:00:02 +0200

    tests for trace serialization

--- rpc/src/v1/types/trace.rs
+++ rpc/src/v1/types/trace.rs
@@ -140,10 +140,15 @@ pub struct Trace {
 	depth: U256,
 	action: Action,
 	result: Res,
-	trace_number: U256,
-	transaction_number: U256,
+	#[serde(rename="tracePosition")]
+	trace_position: U256,
+	#[serde(rename="transactionPosition")]
+	transaction_position: U256,
+	#[serde(rename="transactionHash")]
 	transaction_hash: H256,
+	#[serde(rename="blockNumber")]
 	block_number: U256,
+	#[serde(rename="blockHash")]
 	block_hash: H256,
 }
 
@@ -155,8 +160,8 @@ impl From<LocalizedTrace> for Trace {
 			depth: From::from(t.depth),
 			action: From::from(t.action),
 			result: From::from(t.result),
-			trace_number: From::from(t.trace_number),
-			transaction_number: From::from(t.transaction_number),
+			trace_position: From::from(t.trace_number),
+			transaction_position: From::from(t.transaction_number),
 			transaction_hash: t.transaction_hash,
 			block_number: From::from(t.block_number),
 			block_hash: t.block_hash,
@@ -166,5 +171,74 @@ impl From<LocalizedTrace> for Trace {
 
 #[cfg(test)]
 mod tests {
+	use serde_json;
+	use util::{U256, H256, Address};
+	use v1::types::Bytes;
+	use super::*;
 
+	#[test]
+	fn test_trace_serialize() {
+		let t = Trace {
+			parent: Some(U256::from(1)),
+			children: vec![U256::from(2), U256::from(3)],
+			depth: U256::from(0),
+			action: Action::Call(Call {
+				from: Address::from(4),
+				to: Address::from(5),
+				value: U256::from(6),
+				gas: U256::from(7),
+				input: Bytes::new(vec![0x12, 0x34]),
+			}),
+			result: Res::Call(CallResult {
+				gas_used: U256::from(8),
+				output: Bytes::new(vec![0x56, 0x78]),
+			}),
+			trace_position: U256::from(10),
+			transaction_position: U256::from(11),
+			transaction_hash: H256::from(12),
+			block_number: U256::from(13),
+			block_hash: H256::from(14),
+		};
+		let serialized = serde_json::to_string(&t).unwrap();
+		assert_eq!(serialized, r#"{"parent":"0x01","children":["0x02","0x03"],"depth":"0x00","action":{"call":{"from":"0x0000000000000000000000000000000000000004","to":"0x0000000000000000000000000000000000000005","value":"0x06","gas":"0x07","input":"0x1234"}},"result":{"call":{"gasUsed":"0x08","output":"0x5678"}},"tracePosition":"0x0a","transactionPosition":"0x0b","transactionHash":"0x000000000000000000000000000000000000000000000000000000000000000c","blockNumber":"0x0d","blockHash":"0x000000000000000000000000000000000000000000000000000000000000000e"}"#);
+	}
+
+	#[test]
+	fn test_action_serialize() {
+		let actions = vec![Action::Call(Call {
+			from: Address::from(1),
+			to: Address::from(2),
+			value: U256::from(3),
+			gas: U256::from(4),
+			input: Bytes::new(vec![0x12, 0x34]),
+		}), Action::Create(Create {
+			from: Address::from(5),
+			value: U256::from(6),
+			gas: U256::from(7),
+			init: Bytes::new(vec![0x56, 0x78]),
+		})];
+
+		let serialized = serde_json::to_string(&actions).unwrap();
+		assert_eq!(serialized, r#"[{"call":{"from":"0x0000000000000000000000000000000000000001","to":"0x0000000000000000000000000000000000000002","value":"0x03","gas":"0x04","input":"0x1234"}},{"create":{"from":"0x0000000000000000000000000000000000000005","value":"0x06","gas":"0x07","init":"0x5678"}}]"#);
+	}
+
+	#[test]
+	fn test_result_serialize() {
+		let results = vec![
+			Res::Call(CallResult {
+				gas_used: U256::from(1),
+				output: Bytes::new(vec![0x12, 0x34]),
+			}),
+			Res::Create(CreateResult {
+				gas_used: U256::from(2),
+				code: Bytes::new(vec![0x45, 0x56]),
+				address: Address::from(3),
+			}),
+			Res::FailedCall,
+			Res::FailedCreate,
+		];
+
+		let serialized = serde_json::to_string(&results).unwrap();
+		assert_eq!(serialized, r#"[{"call":{"gasUsed":"0x01","output":"0x1234"}},{"create":{"gasUsed":"0x02","code":"0x4556","address":"0x0000000000000000000000000000000000000003"}},{"failedCall":[]},{"failedCreate":[]}]"#);
+	}
 }
