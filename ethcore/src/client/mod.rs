7a0e2f971f7315ee30da43e9675f5d0ff120c80b
debris <marek.kotewicz@gmail.com>
2016-04-26 21:51:09 +0200

    fix compiling parity after merge

--- ethcore/src/client/mod.rs
+++ ethcore/src/client/mod.rs
@@ -23,7 +23,7 @@ mod test_client;
 mod trace;
 
 pub use self::client::*;
-pub use self::config::{ClientConfig, BlockQueueConfig, BlockChainConfig};
+pub use self::config::{ClientConfig, BlockQueueConfig, BlockChainConfig, Switch};
 pub use self::ids::{BlockId, TransactionId, UncleId, TraceId};
 pub use self::test_client::{TestBlockChainClient, EachBlockWith};
 pub use self::trace::Filter as TraceFilter;
--- parity/configuration.rs
+++ parity/configuration.rs
@@ -26,7 +26,7 @@ use die::*;
 use util::*;
 use util::keys::store::AccountService;
 use util::network_settings::NetworkSettings;
-use ethcore::client::{append_path, get_db_path, ClientConfig};
+use ethcore::client::{append_path, get_db_path, ClientConfig, Switch};
 use ethcore::ethereum;
 use ethcore::spec::Spec;
 use ethsync::SyncConfig;
@@ -194,9 +194,9 @@ impl Configuration {
 			}
 		}
 		client_config.tracing.enabled = match self.args.flag_tracing.as_str() {
-			"auto" => None,
-			"on" => Some(true),
-			"off" => Some(false),
+			"auto" => Switch::Auto,
+			"on" => Switch::On,
+			"off" => Switch::Off,
 			_ => { die!("Invalid tracing method given!") }
 		};
 		client_config.pruning = match self.args.flag_pruning.as_str() {
